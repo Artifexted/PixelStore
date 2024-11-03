89018ddfce99ab0a7f8168657cd55d3adce1923c
Artifexted <artifexted@gmail.com>
2024-11-03 15:16:15 -0300

    actualización de dependencias y arreglos de header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,42 +2,49 @@ import React from 'react';
 import Logo from '../../img/pixelstore.png';
 import CartWidget from './CartWidget';
 import { NavLink } from 'react-router-dom';
+import styles from './Header.module.css';
 
 const Header = () => {
     return (
         <header>
-            <nav className='navbar navbar-light bg-light'>
-                <div className='container-fluid'>
-                    <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-                        <span className='navbar-toggler-icon'></span>
+            <nav className={`navbar ${styles.navbar} navbar-expand-lg navbar-light bg-light`}>
+                <div className="container-fluid d-flex justify-content-between">
+                    <NavLink className="navbar-brand" to="/">
+                        <img className="logo" src={Logo} alt="logo de PixelStore" width="30" height="30" />
+                        PixelStore
+                    </NavLink>
+                    <button 
+                        className={`${styles.navbarToggler} navbar-toggler`} 
+                        type="button" 
+                        data-bs-toggle="collapse" 
+                        data-bs-target="#navbarNav" 
+                        aria-controls="navbarNav" 
+                        aria-expanded="false" 
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className='collapse navbar-collapse' id='navbarNav'>
-                        <ul className='navbar-nav'>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='/'>Inicio</NavLink>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0 text-center">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/">Inicio</NavLink>
                             </li>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='/cat/mb'>Motherboards</NavLink>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/cat/mb">Motherboards</NavLink>
                             </li>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='/cat/proce'>Procesadores</NavLink>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/cat/proce">Procesadores</NavLink>
                             </li>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='/cat/ram'>Memorias RAM</NavLink>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/cat/ram">Memorias RAM</NavLink>
                             </li>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='/cat/video'>Placas de Video</NavLink>
-                            </li>
-                            <li className='nav-item'>
-                                <NavLink className='nav-link' to='/item/:itemID'>Detalle</NavLink>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/cat/video">Placas de Video</NavLink>
                             </li>
                         </ul>
                     </div>
-                    
-                    <a className='navbar-brand' href='index.html'><img className='logo' src={Logo} alt='logo de PixelStore' /> PixelStore</a>
-
-                    <NavLink className='nav-link' to='/cart'>
-                        <button type='button' className='col-1 btn border-0 p-0'>
+                    <NavLink className={styles.cart} to="/cart">
+                        <button type="button" className="btn btn-outline-primary">
                             <CartWidget />
                         </button>
                     </NavLink>
--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyARP0BuQRB7l0N75ZlnfSmFAnZc__3TiUY",
-  authDomain: "ros-pixelstore.firebaseapp.com",
-  projectId: "ros-pixelstore",
-  storageBucket: "ros-pixelstore.appspot.com",
-  messagingSenderId: "725360405701",
-  appId: "1:725360405701:web:85f41f780b0ed7acd254ac"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
